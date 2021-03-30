eaf504ebf57ee33517d36adafa7129c0f47b6831
lzlocki <lzlocki@poczta.fm>
2021-03-30 18:04:09 +0200

    Add DTOs

---
+++ FlexOffice.Api/Dto/LocationDTO.cs
@@ -0,0 +1,13 @@
+namespace FlexOffice.Api.Dto
+{
+    public class LocationDTO
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string UnitLocation { get; set; }
+        public string OfficeName { get; set; }
+        public string OfficeNumber { get; set; }
+        public string ShortLocationDescription { get; set; }
+        public string UrlPhoto { get; set; }
+    }
+}
---
+++ FlexOffice.Api/Dto/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace FlexOffice.Api.Dto
+{
+    public class UserDTO
+    {
+       public int Id { get; set; } 
+       public string UserLog { get; set; }
+       public string Email { get; set; }
+    }
+}
